6fc8ad849e3a714e1d39777b7533b6976bb25128
Aaron Conole <Aaron_Conole@azimuthsystems.com>
2014-11-24 18:30:47 -0500

    Additionally, provide nodejs-repl by default, and fix the bogus cons

--- simple-emacs-web-devel.el
+++ simple-emacs-web-devel.el
@@ -4,14 +4,15 @@
 (require 'web-mode)
 (require 'impatient-mode)
 (require 'js2-refactor)
+(require 'nodejs-repl)
 
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" .web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" .web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 ; set up js2r mode with C-c C-m <chord>
 (js2r-add-keybindings-with-prefix "C-c C-m")
